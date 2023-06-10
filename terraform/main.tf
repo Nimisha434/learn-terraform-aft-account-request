4df5441e079dce2be76051297a9619f14ef9991b
nimisha434 <nimisha.shah434@gmail.com>
2023-06-10 19:20:39 +0100

    Multiple account requests

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,33 +26,7 @@ module "Trade_Dev" {
   account_customizations_name = "Trade_Dev"
 }
 
-module "Trade_test" {
-  source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "joe+test@onmulticloud.com"
-    AccountName               = "Trade_test"
-    ManagedOrganizationalUnit = "Custom"
-    SSOUserEmail              = "joe+test@onmulticloud.com"
-    SSOUserFirstName          = "Trade_test"
-    SSOUserLastName           = "test"
-  }
-
-  account_tags = {
-    "Trade Cloud" = "test"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "Trade_test"
-}
 module "Trade_Prod" {
   source = "./modules/aft-account-request"
 
